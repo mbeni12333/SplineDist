e7ffdaf3e6191c519a5cce88997fa4ed58dd94ae
Benimam Mounib <mbeni12333@gmail.com>
2022-01-07 14:38:27 +0100

    Clean up, added kernel size as a parameter

--- src/Models/splinedist.py
+++ src/Models/splinedist.py
@@ -19,7 +19,7 @@ from torchvision.utils import make_grid
 
 
 ###########################################################################
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 MAX_CONTOUR_SIZE = 291
 
@@ -27,19 +27,6 @@ colors = [ list(map(lambda x: x/255, getRandomColor())) for i in range(256*256+1
 ################################# Helper Functions ################################################################
 
 
-# def matchInstances(postProcessed, target):
-
-#     cost = cdist(postProcessed, target, metric=nms.fast.polygon_iou)
-
-#     rowId, colId = linear_sum_assignment(cost)
-
-
-
-#     TP = []
-#     FP = []
-#     FN = []
-
-#     return TP, FP, FN
 
 
 def computeDistanceBetweenInstance(contour1, contour2):
@@ -67,19 +54,7 @@ def computeDistanceBetweenInstance(contour1, contour2):
     # return cost[rowId, colId].sum()
 
 # #################### TO REMOVE ################################
-# def convertOutputsToControlPoints(angles, distances, size=256):
-#     """
-#     """
-#     controlPoints = torch.stack((torch.cos(angles)*distances,
-#                                  torch.sin(angles)*distances), dim=2)
-#     x = torch.arange(size)
-#     y = torch.arange(size)
-#     xx, yy = torch.meshgrid(x, y)
-#     shifts = torch.stack((xx, yy), dim=0)
 
-#     controlPoints = controlPoints + shifts
-
-#     return controlPoints
 # #################### TO REMOVE ################################
 
 def polygon_iou(poly1, poly2, useCV2=True):
@@ -115,20 +90,26 @@ def polygon_iou(poly1, poly2, useCV2=True):
 def computeContourLoss(objectProbas, contours, targetObjectProbas, targetContours, nms_threshold=0.7, obj_threshold=0.7, lambda2=1.0, shifts=None):
     """
     """
-
+    targetObjectProbas = targetObjectProbas.squeeze(1)
     mask = (targetObjectProbas > 0)
+    mask_norm = mask.flatten(1).sum(1).reshape(-1, 1, 1)
+    N = targetObjectProbas.shape[0]
+    
 
-    term1 = (targetObjectProbas * mask * torch.abs(contours - targetContours)).mean()
-    term2 = (1-mask) * torch.abs(contours)
 
-    loss = term1/mask.mean() + term2
+    
+    term1 = (targetObjectProbas * mask * torch.abs(contours - targetContours).mean((-1, -2))/mask_norm).sum()/N
+    term2 = ((~mask) * torch.abs(contours - shifts.permute(1, 2, 0).reshape(1, 256, 256, 1, 2)).mean((-1, -2))/(256*256-mask_norm)).sum()/N
+    
+    #print(f"mask {mask.shape},\
+    #mask_norm {mask_norm},\
+    #term1 {term1},\
+    #term2 {term2}")
+
+    loss = term1 + lambda2*term2
 
 
     return loss
-
-
-
-
 
 
 # def computeContourLoss(objectProbas, contours, targetObjectProbas, targetContours, nms_threshold=0.7, obj_threshold=0.7, lambda2=1.0, shifts=None):
@@ -279,7 +260,9 @@ class SplineDist(pl.LightningModule):
                  splineBasis=3,
                  lambda1=1,
                  lambda2=0.1,
-                 size=256):
+                 size=256,
+                 kernel_size=1,
+                 device="cuda:2"):
         """
         """
         # initialize the parent class
@@ -292,18 +275,18 @@ class SplineDist(pl.LightningModule):
         
         self.objectFinder = nn.Conv2d(num_features,
                                       num_classes,
-                                      kernel_size=1,
-                                      padding=0)
+                                      kernel_size=kernel_size,
+                                      padding=kernel_size//2)
 
         self.ControlPointsAngleRegressor = nn.Conv2d(num_features,
                                                      num_control_points,
-                                                     kernel_size=1,
-                                                     padding=0)
+                                                     kernel_size=kernel_size,
+                                                     padding=kernel_size//2)
 
         self.ControlPointsDistanceRegressor = nn.Conv2d(num_features,
                                                         num_control_points,
-                                                        kernel_size=1,
-                                                        padding=0)
+                                                        kernel_size=kernel_size,
+                                                        padding=kernel_size//2)
 
         x = torch.arange(size)
         y = torch.arange(size)
@@ -320,7 +303,6 @@ class SplineDist(pl.LightningModule):
         self.B3M = getBsplineMatrix(numSamples=MAX_CONTOUR_SIZE,
                                     degree=splineBasis,
                                     numControlPoints=num_control_points+3).float().to(device)
-    
     
     @staticmethod
     def add_model_specific_args(parent_parser):
@@ -342,7 +324,6 @@ class SplineDist(pl.LightningModule):
         """
         """
         
-
         
         features = self.segmentationBackbone(x)
         objectProbas = self.objectFinder(features)
@@ -357,10 +338,43 @@ class SplineDist(pl.LightningModule):
         return objectProbas, angles, distances, controlPoints
 
 
-    def matchInstances():
+    def matchInstances(self, selectedContours, selectedScores, targetContours):
         """
         """
-        pass
+        # for increasing thresholds, compute the IoU pairwise between selected and target
+        cost = np.zeros(selectedContours.shape[0], targetContours.shape[0])
+        for i in range(selectedContours.shape[0]):
+            for j in range(targetContours.shape[0]):
+                cost[i, j] = polygon_iou(selectedContours[i], targetContours[j])
+        
+        # solve linear assignement problem
+        values, selectedIds = linear_assignment(cost)
+
+
+        precisions, recalls = [], []
+        thresholds = np.linspace(0, 1, 20)
+
+        for threshold in thresholds:
+            # compute true positifs, false negatives, false negatives
+            TP = (values>=threshold).sum()
+            FN = len(set(range(targetContours.shape[0])) - TP)
+            FP = (values<threshold).sum()
+            # compute precision, recall, f1
+            precision = TP/(TP+FP)
+            recall = TP/(TP+FN)
+
+            precisions.append(precision)
+            recalls.append(recall)
+
+        # # find the best threshold
+        # bestThreshold = thresholds[np.argmax(precisions)]
+        # # compute the best precision and recall
+        # bestPrecision = precisions[np.argmax(precisions)]
+        # bestRecall = recalls[np.argmax(precisions)]
+
+
+        return zip(precisions, recalls), thresholds
+
 
     def compute_loss(self, predicted_targets, targets):
         """
@@ -370,11 +384,6 @@ class SplineDist(pl.LightningModule):
         """
         (objectProbas, controlPoints, contours) = predicted_targets
         (targetObjectProbas, targetOverlapProbas, targetContours) = targets
-
-
-        # targetContours = list(map(lambda x: x.to(device), targetContours))
-        # targetObjectProbas = targetObjectProbas.to(device)
-#         controlPoints = controlPoints.permute(0, 3, 4, 1, 2)
 
         # loss 1
 
@@ -392,14 +401,6 @@ class SplineDist(pl.LightningModule):
 
         return finalLoss
 
-
-
-        
-
-    # def on_train_start(self):
-    #     """
-    #     """
-
         
         
     def training_step(self, batch, batch_idx):
@@ -413,7 +414,7 @@ class SplineDist(pl.LightningModule):
         objectProbas, angles, distances, controlPoints = self(images)
         
         contours = getContourSamples(controlPoints, self.B3M)
-        contours = contours.permute(1, 2, 3, 0, 4).flatten(1, 2)#reshape(objectProbas.shape[0], -1, MAX_CONTOUR_SIZE, 2)
+        contours = contours.permute(1, 2, 3, 0, 4)#.flatten(1, 2)#reshape(objectProbas.shape[0], -1, MAX_CONTOUR_SIZE, 2)
         
         predicted_targets = (objectProbas, controlPoints, contours)
         loss = self.compute_loss(predicted_targets, targets)
@@ -429,7 +430,7 @@ class SplineDist(pl.LightningModule):
         objectProbas, angles, distances, controlPoints = self(images)
         
         contours = getContourSamples(controlPoints, self.B3M)
-        contours = contours.permute(1, 2, 3, 0, 4).flatten(1, 2)#reshape(objectProbas.shape[0], -1, MAX_CONTOUR_SIZE, 2)
+        contours = contours.permute(1, 2, 3, 0, 4)#.flatten(1, 2)#reshape(objectProbas.shape[0], -1, MAX_CONTOUR_SIZE, 2)
         
         predicted_targets = (objectProbas, controlPoints, contours)
 
@@ -441,22 +442,22 @@ class SplineDist(pl.LightningModule):
         if(batch_idx == 0):
             ####################################################
             
-            tsb.add_histogram("objectFinder", self.objectFinder.weight, self.step)
-            tsb.add_histogram("AnglesRegressor", self.ControlPointsAngleRegressor.weight, self.step)
-            tsb.add_histogram("DistanceRegressor", self.ControlPointsDistanceRegressor.weight, self.step)
+            tsb.add_histogram("Weights/objectFinder", self.objectFinder.weight, self.step)
+            tsb.add_histogram("Weights/AnglesRegressor", self.ControlPointsAngleRegressor.weight, self.step)
+            tsb.add_histogram("Weights/DistanceRegressor", self.ControlPointsDistanceRegressor.weight, self.step)
             
             
             
-            tsb.add_histogram('distance', distances, self.step)
-            tsb.add_histogram('angle', angles, self.step)
-            tsb.add_histogram('objectProbas', objectProbas, self.step)
+            tsb.add_histogram('Values/distances', distances, self.step)
+            tsb.add_histogram('Values/angles', angles, self.step)
+            tsb.add_histogram('Values/objectProbas', objectProbas, self.step)
             ####################################################
             fig = plt.figure(figsize=(10, 10))
             objectProbas = torch.sigmoid(objectProbas)
             im = objectProbas[0, 0].cpu().numpy()
             plt.imshow(im, cmap="gnuplot2")
             plt.colorbar()
-            tsb.add_figure("ObjectProbas", fig, self.step)      
+            tsb.add_figure("Sample/objectProbas", fig, self.step)      
             
             ###################################################
             fig = plt.figure(figsize=(10, 10))
@@ -465,21 +466,24 @@ class SplineDist(pl.LightningModule):
             ct = controlPoints[0].cpu().numpy()
             for i in range(0, 256, 8):
                 for j in range(0, 256, 8):
-                    ax.fill(ct[i, j, :, 1], ct[i, j, :, 0], 'o-')
+                    ax.fill(ct[i, j, :, 0], ct[i, j, :, 1], 'o-')
             # plot_to_tensorboard(tsb, fig, None, f"densePredictionsConvexHull")
-            tsb.add_figure(f"densePredictionsConvexHull", fig, self.step)
+            tsb.add_figure(f"Sample/densePredictionsConvexHull", fig, self.step)
             ###################################################
             
             
-            for i in range(8):
+            for i in range(self.hparams["num_control_points"]):
+                circular_angle = np.mod(angles[0, i].detach().cpu().numpy() + np.pi, 2*np.pi)
+                
                 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 7))
                 ax1.imshow(distances[0, i].detach().cpu().numpy())
-                ax2.imshow(angles[0, i].detach().cpu().numpy())
+                ax2.imshow(circular_angle, cmap="gist_rainbow")
                 # plot_to_tensorboard(tsb, fig, None, f"distance_angles{i}")
-                tsb.add_figure(f"distance_angles{i}", fig, self.step)
+                tsb.add_figure(f"Sample/distance_angles{i}", fig, self.step)
             
-
+            contours = contours.flatten(1, 2)
             threshold = self.hparams["object_threshold"]
+            nms_threshold = self.hparams["nms_threshold"]
             contours = contours[0].reshape(-1, MAX_CONTOUR_SIZE, 2)
 
             xmin = torch.amin(contours[:, :, 1], dim=-1) 
@@ -500,37 +504,54 @@ class SplineDist(pl.LightningModule):
             selectedIds = selectedIds.detach().cpu().numpy()
 
             rects = bboxes[selectedIds]
-
-
-            image2 = denormalize(images[0].detach().cpu()).numpy().transpose(1, 2, 0)
-            ctrs = contours[scores>threshold][selectedIds].detach().cpu().numpy()
             
+            
+#             scores = objectProbas[0].flatten(1).cpu().detach().numpy()
+#             sortedInstances = np.argsort(scores, axis=1).copy()
+
+#             til = [np.searchsorted(scores[0, sortedInstances[0]], threshold)]
+
+#             contours2 = [contours[til[0]:].detach().cpu().numpy()]
+
+#             print(len(contours2[0]))
+#             postProcessed = nonMaximumSuppresion(scores[0, sortedInstances[0, til[0]:]],
+#                                                 contours2[0],
+#                                                 threshold,
+#                                                 nms_threshold)
+
+#             contoursSelected, selectedIds = postProcessed
+#             selectedIds = np.array(selectedIds)
+
+
+            #image2 = denormalize(images[0].detach().cpu()).numpy().transpose(1, 2, 0)
+    
+            image2 = np.uint8(denormalize(images[0].detach().cpu()).numpy().transpose(1, 2, 0)*255)
+
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        #     image2 = clahe.apply(image2)
+            img_yuv = cv2.cvtColor(image2, cv2.COLOR_RGB2YUV)
+
+            # equalize the histogram of the Y channel
+            img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0])
+
+            # convert the YUV image back to RGB format
+            image2 = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB)
+    
+    
+            ctrs = contours[scores>threshold][selectedIds].detach().cpu().numpy()
+#             ctrs = contoursSelected
 
 
             fig = plt.figure(figsize=(10, 10))
             ax4 = fig.gca()
-
-            # instances, colors = getInstancesImageFromContours(objectContours)
-#             ax4.imshow(denormalize(img[0]).cpu().numpy().transpose(1, 2, 0))
             ax4.imshow(image2)
-            # ax4.imshow(instances, alpha=0.3)
-#             colors = [ list(map(lambda x: x/255, getRandomColor())) for j in range(len(contours))]
 
-            for j, box in enumerate(rects):
-
-                xmin, ymin, xmax, ymax = box
-                rect = patches.Rectangle((xmin, ymin),
-                                          xmax-xmin,
-                                          ymax-ymin,
-                                          linewidth=1, color=colors[j], alpha=0.3)
-
-                ax4.add_patch(rect)
-
-                ax4.fill(ctrs[j][:, 1], ctrs[j][:, 0], color=colors[j], alpha=0.3)
-                ax4.plot(ctrs[j][:, 1], ctrs[j][:, 0],color=colors[j], linewidth=2, linestyle='dashed')
+            for j, ct in enumerate(ctrs):
+                ax4.fill(ctrs[j][:, 0], ctrs[j][:, 1], color=colors[j], alpha=0.3)
+                ax4.plot(ctrs[j][:, 0], ctrs[j][:, 1],color=colors[j], linewidth=2, linestyle='dashed')
 
 
-            tsb.add_figure(f"Instances", fig, self.step)
+            tsb.add_figure(f"Sample/Instances", fig, self.step)
                 
             
             self.step += 1
@@ -542,8 +563,9 @@ class SplineDist(pl.LightningModule):
         """
 
         """
-        optimizer = optim.Adam(self.parameters(), lr=self.hparams["learning_rate"])
-#         scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
-        lr_schedulers = {"scheduler": scheduler, "monitor": "train_loss"}
-        return [optimizer], lr_schedulers
+        optimizer = optim.Adam(self.parameters(), lr=self.hparams["learning_rate"], weight_decay=0.0001)
+#         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 40)
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+#         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=5)
+#         lr_schedulers = {"scheduler": scheduler, "monitor": "val_loss"}
+        return [optimizer], scheduler#, lr_schedulers
